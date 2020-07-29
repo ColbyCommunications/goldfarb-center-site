26708fa196b87aa01efdfd60feaeab8a6311c967
Brandon K. Waltz <bkwaltz@colby.edu>
2020-07-29 16:49:37 -0400

    hmm

--- scripts/dependencies-run-install-build.sh
+++ scripts/dependencies-run-install-build.sh
@@ -13,35 +13,49 @@ npm install
 gulp
 cd -
 
-printf "Plugins... \n"
-NPM_PLUGIN_DIRS=`ls web/wp-content/plugins/colby-*/src/@(index.js)` # Saves it to a variable
-for NPMPLUGINDIR in $NPM_PLUGIN_DIRS; do
-  NPMPLUGINDIR=`dirname $NPMPLUGINDIR`
-  NPMPLUGINDIR_PRUNED=${NPMPLUGINDIR:0:$((${#NPMPLUGINDIR}-3))}
-  cd $NPMPLUGINDIR_PRUNED
-  printf "Installing NPM dependencies for ${NPMPLUGINDIR_PRUNED}... \n"
-  npm install
-  printf "Running build for ${NPMPLUGINDIR_PRUNED}... \n"
-  npm run build
-  cd -
-done
+cd web/wp-content/themes/colby-base-theme
+composer install
+composer dump-autoload
+npm install
+npm run build:production
+cd -
+
+cd web/wp-content/themes/colby-goldfarb-theme
+composer install
+composer dump-autoload
+npm install
+npm run build:production
+cd -
+
+# printf "Plugins... \n"
+# NPM_PLUGIN_DIRS=`ls web/wp-content/plugins/colby-*/src/@(index.js)` # Saves it to a variable
+# for NPMPLUGINDIR in $NPM_PLUGIN_DIRS; do
+#   NPMPLUGINDIR=`dirname $NPMPLUGINDIR`
+#   NPMPLUGINDIR_PRUNED=${NPMPLUGINDIR:0:$((${#NPMPLUGINDIR}-3))}
+#   cd $NPMPLUGINDIR_PRUNED
+#   printf "Installing NPM dependencies for ${NPMPLUGINDIR_PRUNED}... \n"
+#   npm install
+#   printf "Running build for ${NPMPLUGINDIR_PRUNED}... \n"
+#   npm run build
+#   cd -
+# done
 
 
-printf "Sage Themes... \n"
-NPM_THEME_DIRS=`ls web/wp-content/themes/colby-*/resources/assets/scripts/@(main.js)` # Saves it to a variable
-for NPMTHEMEDIR in $NPM_THEME_DIRS; do
-  NPMTHEMEDIR=`dirname $NPMTHEMEDIR`
-  # NPMTHEMEDIR_PRUNED=${NPMTHEMEDIR:0:$((${#NPMTHEMEDIR}-3))}
-  cd $NPMTHEMEDIR
-  printf "Installing Composer dependencies for ${NPMTHEMEDIR_PRUNED}... \n"
-  composer install
-  composer dump-autoload
-  printf "Installing NPM dependencies for ${NPMTHEMEDIR_PRUNED}... \n"
-  npm install
-  printf "Running build for ${NPMTHEMEDIR_PRUNED}... \n"
-  npm run build:production
-  cd -
-done
+# printf "Sage Themes... \n"
+# NPM_THEME_DIRS=`ls web/wp-content/themes/colby-*/resources/assets/scripts/@(main.js)` # Saves it to a variable
+# for NPMTHEMEDIR in $NPM_THEME_DIRS; do
+#   NPMTHEMEDIR=`dirname $NPMTHEMEDIR`
+#   # NPMTHEMEDIR_PRUNED=${NPMTHEMEDIR:0:$((${#NPMTHEMEDIR}-3))}
+#   cd $NPMTHEMEDIR
+#   printf "Installing Composer dependencies for ${NPMTHEMEDIR_PRUNED}... \n"
+#   composer install
+#   composer dump-autoload
+#   printf "Installing NPM dependencies for ${NPMTHEMEDIR_PRUNED}... \n"
+#   npm install
+#   printf "Running build for ${NPMTHEMEDIR_PRUNED}... \n"
+#   npm run build:production
+#   cd -
+# done
 
 # npm install
 shopt -u extglob
