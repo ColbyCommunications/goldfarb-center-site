3c2da7e3c9b89d2be68f259a87cb947984ab9d87
Brandon K. Waltz <bwaltz6@gmail.com>
2021-08-10 14:25:13 -0400

    remove unnecessary parts from build script

--- scripts/dependencies-run-install-build.sh
+++ scripts/dependencies-run-install-build.sh
@@ -7,18 +7,6 @@ shopt -s extglob # Turns on extended globbing
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
-# base-install
-cd web/wp-content/themes/baseinstall
-npm install
-gulp
-cd -
-
-cd web/wp-content/themes/colby-base-theme
-composer install
-composer dump-autoload
-npm install
-npm run build:production
-cd -
 
 cd web/wp-content/themes/colby-goldfarb-theme
 composer install
